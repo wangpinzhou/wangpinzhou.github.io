338b786cc53755af3d2edaa82c1378433d6e161e
wangpinzhou <wangpinzhou@qq.com>
2020-04-27 22:27:49 +0800

    fix

--- components/cj/w_cj_table.vue
+++ components/cj/w_cj_table.vue
@@ -25,7 +25,7 @@ module.exports = {
     return {
       list: [{}],
       newRow: {
-        Id: Math.random(),
+        id: Math.random(),
         sku: "new row",
         color: "new",
         size: "M",
